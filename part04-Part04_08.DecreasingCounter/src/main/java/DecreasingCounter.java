2861bc1811c917a9f37834fb6646b37916a3ff10
Lucas Sandgren <lucas.sandgren@gmail.com>
2022-04-11 23:33:54 +0200

    passed 04_08 3 parts

--- part04-Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
+++ part04-Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
@@ -8,13 +8,19 @@ public class DecreasingCounter {
     }
 
     public void printValue() {
-        // Do not change this code!
         System.out.println("value: " + this.value);
     }
 
     public void decrement() {
-        // write the method implementation here
-        // the aim is to decrement the value of the counter by one
+        if(this.value>0) {
+            this.value--;
+        }
+        System.out.println(this.value);
+    }
+    public void reset(){
+        this.value = 0;
+        System.out.println(this.value);
+        
     }
 
     // the other methods go here
