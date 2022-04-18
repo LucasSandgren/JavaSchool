0b628aee886b6ec553e687f2950acd881545869c
Lucas Sandgren <lucas@godsent.gg>
2022-04-18 17:50:38 +0200

    passed 04_20

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -4,9 +4,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> bookList = new ArrayList<>();
         
-        // implement here the program that allows the user to enter 
-        // book information and to examine them
-
+        while(true){
+            System.out.println("Title: ");
+            String bookTitle = scanner.nextLine();
+                if(bookTitle.isEmpty()){
+                    break;
+            }
+            System.out.println("Pages: ");
+                int bookPages = Integer.valueOf(scanner.nextLine());
+            
+            System.out.println("Publication year: ");
+                int pubYear = Integer.valueOf(scanner.nextLine());
+                
+                bookList.add(new Book (bookTitle, bookPages, pubYear));
+        } //end of while statement
+        
+        System.out.println("What information will be printed?");
+            String whatToPrint = scanner.nextLine();
+            
+        for (Book book : bookList){
+            if(whatToPrint.equals("everything")){
+                System.out.println(book.getInstanceBook() + ", " + book.getInstancePages() + " pages, " + book.getInstanceYear());
+            }
+            else if(whatToPrint.equals("name")){
+                System.out.println(book.getInstanceBook());
+            }
+            
+        }
     }
 }
+
