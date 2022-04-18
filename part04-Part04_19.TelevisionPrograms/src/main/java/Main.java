b234fad49ff4c2a42e5ba1a151d3362ceca961fb
Lucas Sandgren <lucas@godsent.gg>
2022-04-18 15:49:05 +0200

    passed 04_19

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -8,6 +8,29 @@ public class Main {
 
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        while(true){
+            System.out.println("Name: ");
+            String program = scanner.nextLine();
+            if (program.isEmpty()){
+                break;
+        }
+            System.out.println("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+            
+            programs.add(new TelevisionProgram (program, duration));
+        }  
+            
+        System.out.println("Programs maximum duration?");
+            int durationLimit = Integer.valueOf(scanner.nextLine());
+            
+            for (TelevisionProgram tv : programs){
+                if (tv.getDuration() <= durationLimit){
+                    System.out.println(tv);
 
+        
+                }
+        }
     }
 }
+
