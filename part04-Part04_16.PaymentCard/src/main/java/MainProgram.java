a485de46b53164c386ab8e961833aa5c5c8423f1
Lucas Sandgren <lucas@godsent.gg>
2022-04-18 14:31:56 +0200

    passed 04_15

--- part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
+++ part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
@@ -1,11 +1,31 @@
-
+import java.util.Scanner;
 public class MainProgram {
 
     public static void main(String[] args) {
-        // Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         
-        // Here you can write code to test that PaymentCard works as intended
-        // be sure to erase the extra code for the last part of the exercise!
-
+        PaymentCard paulsCard = new PaymentCard(20); // Creates Pauls card
+        PaymentCard mattsCard = new PaymentCard(30); // Creates Matts card
+        
+        paulsCard.eatHeartily(); // Paul eats the expensive meal
+        mattsCard.eatAffordably(); // Matt eats the cheap meal
+        
+        System.out.println("Paul: The card has a balance of " + paulsCard + " euros");
+        System.out.println("Matt: The card has a balance of " + mattsCard + " euros");
+        
+        paulsCard.addMoney(20); //add 20 to Pauls card
+        mattsCard.eatHeartily();
+        
+        System.out.println("Paul: The card has a balance of " + paulsCard + " euros"); // Prints current balance
+        System.out.println("Matt: The card has a balance of " + mattsCard + " euros");
+        
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        
+        mattsCard.addMoney(50);
+        
+        System.out.println("Paul: The card has a balance of " + paulsCard + " euros"); // Prints current balance
+        System.out.println("Matt: The card has a balance of " + mattsCard + " euros");
+        
     }
 }
