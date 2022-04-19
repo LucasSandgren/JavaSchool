fa1ed9b360612de5fcea56e85d9099c132e8859a
Lucas Sandgren <lucas@godsent.gg>
2022-04-19 14:15:30 +0200

    passed 04_24

--- part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -1,10 +1,20 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PrintingAFile {
 
     public static void main(String[] args) {
-
+        try (Scanner sc = new Scanner(Paths.get("data.txt"))){
+            while(sc.hasNextLine()){
+                String textRead = sc.nextLine();
+                System.out.println(textRead);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(PrintingAFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
