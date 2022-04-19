6b06e2cfc8dd29de6571f3bd3a7fcc20707ae514
Lucas Sandgren <lucas@godsent.gg>
2022-04-19 12:45:22 +0200

    passed 04_21

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -4,7 +4,18 @@ import java.util.Scanner;
 public class NumberOfStrings {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input: ");
+        int number=0;
+        while(true){
+            String inputCount = sc.nextLine();
+            number++;
+            if(inputCount.equals("end")){
+                number--;
+                break;
+            }
+            
+        }
+        System.out.println(number);
     }
 }
