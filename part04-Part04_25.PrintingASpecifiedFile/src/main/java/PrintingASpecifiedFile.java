ef78290b8ef55a5645339c1ade55f437d842b682
Lucas Sandgren <lucas@godsent.gg>
2022-04-19 14:49:47 +0200

    passed 04_25

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -1,11 +1,26 @@
-
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PrintingASpecifiedFile {
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-
+        String fileName;
+        System.out.println("Which file should have its contents printed?");
+        fileName = scanner.nextLine();
+        findFile(fileName);    
+    }    
+    
+    public static void findFile(String fileName) {
+        try (Scanner SC = new Scanner(Paths.get("fileName"))){
+            while(SC.hasNextLine()){
+                String textRead = SC.nextLine();
+                System.out.println(textRead);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(PrintingASpecifiedFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
