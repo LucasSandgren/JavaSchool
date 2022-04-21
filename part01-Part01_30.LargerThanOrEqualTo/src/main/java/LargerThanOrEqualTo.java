f11cd01d8136edb48964b499a94a696553b7b831
Lucas Sandgren <lucas@godsent.gg>
2022-04-21 16:35:33 +0200

    passed

--- part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -5,6 +5,20 @@ public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("Give the first number:");
+        int firstNumber=scan.nextInt();
+        System.out.println("Give the second number:");
+        int secondNumber=scan.nextInt();
+        if (firstNumber>secondNumber){
+            System.out.println("Greater number is: "+firstNumber);
+        }
+        if(firstNumber<secondNumber){
+            System.out.println("Greater number is: "+secondNumber);
+        }
+        if(firstNumber==secondNumber){
+            System.out.println("The numbers are equal!");
+        }
     }
 }
+
+
