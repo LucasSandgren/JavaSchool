eb2664fb85d45af2bceba99e4a8471c64f434c6a
Lucas Sandgren <lucas@godsent.gg>
2022-04-21 16:26:47 +0200

    passed 01_28

--- part01-Part01_28.Positivity/src/main/java/Positivity.java
+++ part01-Part01_28.Positivity/src/main/java/Positivity.java
@@ -5,7 +5,13 @@ public class Positivity {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // Write your program here
+        System.out.println("Guve a number: ");
+        int input=scan.nextInt();
+        if(input>0){
+            System.out.println("The number is positive.");
+        }else{
+            System.out.println("The number is not positive.");
+        }
+        
     }
 }
