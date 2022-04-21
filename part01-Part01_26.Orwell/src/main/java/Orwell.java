12d93c6d7411c71c8ad5086fc5d1eb7f8214e1ac
Lucas Sandgren <lucas@godsent.gg>
2022-04-21 16:22:08 +0200

    updated

--- part01-Part01_26.Orwell/src/main/java/Orwell.java
+++ part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -5,7 +5,10 @@ public class Orwell {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // Write your program here
+        System.out.println("Give a number: ");
+        int input = scan.nextInt();
+        if(input==1984){
+            System.out.println("Orwell");
+        }
     }
 }
