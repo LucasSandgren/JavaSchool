b931a2a2636254243ca6e450e5264dce9d74a120
Lucas Sandgren <lucas@godsent.gg>
2022-04-21 16:00:27 +0200

    04_28, file works but not passing test for some reason

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,19 +1,45 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NumbersFromAFile {
-
+    private static int count;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("File? ");
-        String file = scanner.nextLine();
-        System.out.print("Lower bound? ");
-        int lowerBound = Integer.valueOf(scanner.nextLine());
-        System.out.print("Upper bound? ");
-        int upperBound = Integer.valueOf(scanner.nextLine());
-
+        ArrayList<String> list = new ArrayList<>(); // Create ArrayList to check ranges against
+        Scanner SC = new Scanner(System.in);
+        
+        System.out.println("File? ");
+        String file = SC.nextLine(); // File input to search for
+        System.out.println("Lower bound? ");
+        int lowerBound = Integer.valueOf(SC.nextLine()); // Lower number input
+        System.out.println("Upper bound? ");
+        int upperBound = Integer.valueOf(SC.nextLine()); // Upper number input
+    
+    try(Scanner fileScan = new Scanner(Paths.get(file))){
+            while(fileScan.hasNextLine()){
+                list.add(fileScan.nextLine()); // add next line to ArrayList line
+            }
+        }catch (IOException ex) {
+            System.out.println("Reading the file" + file + "failed");
+        }
+        for (String counter : list){
+            int numberRange=Integer.valueOf(counter); // range between upper and lower bound
+            if(numberRange >=lowerBound && numberRange <=upperBound){
+                count++;
+        }else{
+            continue;
+            }
+        } 
+        System.out.println("Numbers: " + count);
     }
-
 }
+
+        
+       
+    
+    
+
+
+
