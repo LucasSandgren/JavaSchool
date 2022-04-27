cf24e23404bc1e844498b7311ebf27171bb320c3
Lucas Sandgren <lucas@godsent.gg>
2022-04-27 13:24:44 +0200

    passed 04_29

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -1,11 +1,24 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class RecordsFromAFile {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        Scanner SC = new Scanner(System.in);
+        String file = SC.nextLine();
+        
+        try (Scanner fileScan = new Scanner (Paths.get(file))){
+            while(fileScan.hasNextLine()){
+            String line = fileScan.nextLine();
+            
+            String[]parts = line.split(",");
+            String name = parts[0];
+            int age = Integer.valueOf(parts[1]);
+            System.out.println(name + ", " + age + " years");
+            }
 
+    }   
     }
 }
